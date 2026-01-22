cb39397f7e27a863e9b721420f9d345183a891e3
Stacey Grant <stacey.a.grant@gmail.com>
2026-01-21 21:39:37 -0500

    changed tastemakers location

--- server.js
+++ server.js
@@ -17,7 +17,8 @@ const fetch = require('node-fetch');
 const { JSDOM } = require("jsdom");
 const { Readability } = require("@mozilla/readability");
 const nlp = require("compromise");
-const { TASTEMAKERS } = require('../config/tastemakers');
+const { TASTEMAKERS } = require('./config/tastemakers');
+
 
 // --- OpenAI (SDK) ---
 
