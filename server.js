dfee2da7e81598dab07615c22d90f58d06d53d59
Stacey Grant <stacey.a.grant@gmail.com>
2026-01-21 20:17:13 -0500

    added another collecto-desktop route

--- server.js
+++ server.js
@@ -58,9 +58,11 @@ const supabase = createClient(
 // 1) Simple whitelist — NO trailing slashes
 const ALLOWED_ORIGINS = [
   'https://www.cultureschool.org',
+  'https://cultureschool.org',
   'https://cocoboard-preview-html.netlify.app',
   'https://coco-collector.netlify.app',
   'https://collector-desktop.netlify.app',
+  'https://collector-desktop.cultureschool.org',
   'https://collector-mobile.netlify.app',
   'https://explore-cocospark.netlify.app',
   'https://ultimate-viewer.netlify.app',
